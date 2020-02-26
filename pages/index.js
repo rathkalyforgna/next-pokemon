c60dd85712aace6d48389724046ebedd569bc39b
forgna <chhin.rathkalyforgna@gmail.com>
2020-02-26 17:44:40 +0700

    Using paginated data with swr

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,48 @@
+import React from 'react'
 import Head from 'next/head'
-import useSWR from 'swr'
+import useSWR, { useSWRPages } from 'swr'
 import fetcher from '../lib/fetcher'
-import PokemonShort from "../components/pokemon-short";
-
-const URL = `https://pokeapi.co/api/v2/pokemon`
+import PokemonShort from '../components/pokemon-short'
+import useOnScreen from '../hooks/use-on-screen'
 
 const HomePage = () => {
-  const { data } = useSWR(URL, fetcher)
+  const { pages, isLoadingMore, loadMore } = useSWRPages(
+    "pokemon-list",
+    ({ offset, withSWR }) => {
+      const url = offset || "https://pokeapi.co/api/v2/pokemon"
+      const { data } = withSWR(useSWR(url, fetcher))
 
-  if (!data) return <h1>Loading...</h1>
-  
-  const { results } = data;
+      if (!data) return null
+
+      const { results } = data;
+      return results.map(result => (
+        <PokemonShort key={result.name} name={result.name} />
+      ))
+    },
+    SWR => SWR.data.next,
+    []
+  )
+
+  const [infiniteScrollEnabled, setInfiniteScrollEnabled] = React.useState(false)
+
+  const $loadMoreButton = React.useRef(null)
+  const infiniteScrollCount = React.useRef(0)
+  const isOnScreen = useOnScreen($loadMoreButton, "200px")
+
+  React.useEffect(() => {
+    if (!infiniteScrollEnabled || !isOnScreen) return
+
+    loadMore()
+
+    const count = infiniteScrollCount.current;
+
+    if (count + 1 === 3) {
+      setInfiniteScrollEnabled(false)
+      infiniteScrollCount.current = 0
+    } else {
+      infiniteScrollCount.current = count + 1
+    }
+  }, [infiniteScrollEnabled, isOnScreen])
 
   return (
     <>
@@ -20,12 +52,20 @@ const HomePage = () => {
           rel="stylesheet"
         />
       </Head>
-
       <section className="container mx-auto">
-        <div className="-mx-2 flex flex-wrap">
-          {results.map(result => (
-            <PokemonShort key={result.name} name={result.name} />
-          ))}
+        <div className="-mx-2 flex flex-wrap">{pages}</div>
+        <div className="mx-auto mt-10 mb-20 w-1/3">
+          <button
+            ref={$loadMoreButton}
+            className="bg-red-600 border-solid border-2 hover:bg-white border-red-600 text-white hover:text-red-600 font-bold py-2 px-4 rounded-full w-full"
+            disabled={isLoadingMore}
+            onClick={() => {
+              loadMore()
+              setInfiniteScrollEnabled(true)
+            }}
+          >
+            Load More Pokémon
+          </button>
         </div>
       </section>
     </>
